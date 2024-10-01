f44ddc92107ba6070f7e2a4e7ba9db116355ffbd
pruiz5514 <pablo_ruiza@hotmail.com>
2024-09-30 19:39:24 -0500

    se incializa proyecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400", "700","900"], });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={roboto.className}
       >
         {children}
       </body>
